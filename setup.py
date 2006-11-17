d127f5e00fbf8785955bf2c55f8a6a13ea889faa
Chad Whitacre <chad@zetaweb.com>
2006-11-17 20:22:22 +0000

    bumping version number and date

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ classifiers = [
    ]
 
 setup( name = 'aspen'
-     , version = '~~VERSION~~'
+     , version = '0.3'
      , package_dir = {'':'src'}
      , packages = ['aspen']
      , scripts = ['bin/aspen']
