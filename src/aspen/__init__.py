85aaaa9d3dfc4a61bd2017a03b48b7228b26436c
Chad Whitacre <chad@zetaweb.com>
2006-12-04 23:09:44 +0000

    updated doc and spiffified autoindex

--- src/aspen/__init__.py
+++ src/aspen/__init__.py
@@ -87,6 +87,14 @@ def _main(argv):
         server.stop()
 
 
+def wrap(func, *arg, **kw):
+    try:
+        func(*arg, **kw)
+    except:
+        import traceback
+        traceback.print_exc()
+
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
