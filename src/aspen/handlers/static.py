e2c9f4b1baa7db0a5dcdc2d990465c5b816429d7
Chad Whitacre <chad@zetaweb.com>
2007-02-26 20:58:00 +0000

    backing out extraneous changes to static now that we know what the real problem was: issue 92

--- src/aspen/handlers/static.py
+++ src/aspen/handlers/static.py
@@ -14,84 +14,22 @@ from aspen.handlers.autoindex import autoindex
 from aspen.handlers.http import HTTP403
 
 
-# import-time configuration
-# =========================
+# Configure directory browsing.
+# =============================
 
-# CHUNK_SIZE
-# ----------
-
-val = conf.static.get('chunk_size', '8192')
-if not val.isdigit() or (int(val) == 0):
-    raise ConfigError( "chunk_size must be an integer greater than 0"
+AUTOINDEX = conf.static.get('autoindex', None)
+if AUTOINDEX is None:
+    AUTOINDEX = True # default
+elif AUTOINDEX.lower() == 'yes':
+    AUTOINDEX = True
+elif val.lower() == 'no':
+    AUTOINDEX = False
+else:
+    raise ConfigError( "autoindex must be 'yes' or 'no'"
                      , "__/etc/aspen.conf"
                      , -1 # lineno
                       )
-CHUNK_SIZE = int(val)
-
-
-# AUTOINDEX
-# ---------
-
-val = conf.static.get('autoindex', None)
-if val is None:
-    val = True # default
-else:
-    if val.lower() == 'yes':
-        val = True
-    elif val.lower() == 'no':
-        val = False
-    else:
-        raise ConfigError( "autoindex must be 'yes' or 'no'"
-                         , "__/etc/aspen.conf"
-                         , -1 # lineno
-                          )
-AUTOINDEX = val
-
-
-# WSGI return iterable
-# ====================
-
-class Resource(object):
-    """Wrap a file object into an interable.
-
-    The value here is in:
-
-      - closing the file when we're done done with it
-      - logging
-      - serving a raw open() file on Windows doesn't work w/ wsgiserver.py
-
-    For documentation on this last item, look at aspen-users around Jan 31,
-    2007.
-
-    This implementation is borrowed from Quixote. Since it's only a few lines of
-    code we aren't bothering with license documentation.
-
-    """
-
-    def __init__(self, filename):
-        self._fp = open(filename, 'rb')
-        if mode.debdev:
-            print "static resource called"
-            self._start = datetime.datetime.now()
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        chunk_ = self._fp.read(CHUNK_SIZE)
-        if not chunk_:
-            raise StopIteration
-        return chunk_
-
-    def close(self):
-        try:
-            if mode.debdev:
-                elapsed = datetime.datetime.now() - self._start
-                print "static content delivered in %s sec" % str(elapsed)
-            self._fp.close()
-        except Exception, why:
-            if mode.debdev:
-                print "error closing fp: %s" % str(why)
+directory = AUTOINDEX and autoindex or HTTP403
 
 
 # WSGI callable
@@ -109,8 +47,7 @@ def static(environ, start_response):
 
     path = environ['PATH_TRANSLATED']
     if isdir(path):
-        call = AUTOINDEX and autoindex or HTTP403
-        return call(environ, start_response)
+        return directory(environ, start_response)
     else:
         assert isfile(environ['PATH_TRANSLATED']) # sanity check
 
@@ -149,4 +86,4 @@ def static(environ, start_response):
     if status == '304 Not Modified':
         return []
     else:
-        return Resource(path)
+        return open(path, 'rb') # need 'rb' for Windows
