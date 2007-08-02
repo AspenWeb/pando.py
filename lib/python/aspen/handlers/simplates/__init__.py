0d7c5b8d8a14d84b0168e1b3b8e71fce47097b0b
Chad Whitacre <chad@zetaweb.com>
2007-08-02 23:27:08 +0000

    Simplates live!!!!!!!!

--- lib/python/aspen/handlers/simplates/__init__.py
+++ lib/python/aspen/handlers/simplates/__init__.py
@@ -74,7 +74,40 @@ FORM_FEED = chr(12) # ^L, ASCII page break
 
 class Simplate(object):
     """Base class for framework-specific simplate implementations.
+
+    This class is instantiated on import when each framework is available, and
+    is then wired up in handlers.conf.
+
+
+    Django = Django_0_96
+
     """
+
+    response_class = None # override w/ framework's response class
+                          # used for "raise SystemExit" semantics.
+
+    def __call__(self, environ, start_response):
+        """Framework shouldn't override this.
+        """
+
+    def build_template(self, template):
+        """Given a string, return a framework-specific template object.
+        """
+        raise NotImplementedError
+
+    def namespace_script(self, namespace):
+        """Given a dictionary, populate it with framework objects.
+        """
+        raise NotImplementedError
+
+    def namespace_template(self, namespace):
+        """Given an empty dictionary, populate it with framework objects.
+
+        The result of this call is updated with the result of namespace_script,
+        before being used to render the template.
+
+        """
+        raise NotImplementedError
 
 
 
@@ -192,12 +225,3 @@ class Cache(object):
         response = HttpResponse(template.render(template_context))
         del response.headers['Content-Type'] # take this from the extension
         return response
-
-
-
-def wsgi(self, environ, start_response):
-    """WSGI contract.
-    """
-
-
-
