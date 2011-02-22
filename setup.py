bec0e401c2d8285a37992410bf5406c3ecea4233
Chad Whitacre <chad@zetaweb.com>
2011-02-22 14:08:52 -0500

    Suppress spurious warnings on build/install.

--- setup.py
+++ setup.py
@@ -4,27 +4,28 @@ use_setuptools()
 from setuptools import find_packages, setup
 
 
-classifiers = [
-    'Development Status :: 4 - Beta'
-  , 'Environment :: Console'
-  , 'Intended Audience :: Developers'
-  , 'License :: OSI Approved :: MIT License'
-  , 'Natural Language :: English'
-  , 'Operating System :: MacOS :: MacOS X'
-  , 'Operating System :: Microsoft :: Windows'
-  , 'Operating System :: POSIX'
-  , 'Programming Language :: Python'
-  , 'Topic :: Internet :: WWW/HTTP :: HTTP Servers'
-   ]
+classifiers = [ 'Development Status :: 4 - Beta'
+              , 'Environment :: Console'
+              , 'Intended Audience :: Developers'
+              , 'License :: OSI Approved :: MIT License'
+              , 'Natural Language :: English'
+              , 'Operating System :: MacOS :: MacOS X'
+              , 'Operating System :: Microsoft :: Windows'
+              , 'Operating System :: POSIX'
+              , 'Programming Language :: Python'
+              , 'Topic :: Internet :: WWW/HTTP :: HTTP Servers'
+               ]
 
 setup( author = 'Chad Whitacre'
      , author_email = 'chad@zetaweb.com'
      , classifiers = classifiers
-     , description = ('Aspen is an async web app server. '
-                      'Simplates are the thing about it.')
+     , description = ('Aspen is a Python web server. '
+                      'Simplates are the main attraction.')
      , entry_points = { 'console_scripts': ['aspen = aspen.cli:main'] }
      , name = 'aspen'
-     , packages = find_packages() 
+     , packages = find_packages(exclude=[ 'aspen.socket_io'     # not ready for
+                                        , 'aspen.socket_io.*'      # prime time
+                                         ]) 
      , url = 'http://aspen.io/'
      , version = '~~VERSION~~'
      , install_requires = ['diesel == 1.9.7b'] # waiting for public release
