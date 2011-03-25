7da3336ce59ac85dabf232bdfc0369685e460245
Christian Wyglendowski <christian@dowski.com>
2011-03-25 13:13:41 -0400

    Fixed an issue with importing Diesel HTTP headers.
    Diesel seems to store headers in a dict of {str: list} format. Aspen
    does this as well and it was double-nesting the lists received from
    Diesel.
    
    Also tweaked the Mapping.add method to work when items both exist and
    don't exist in the mapping prior to calling add.

--- aspen/http/headers.py
+++ aspen/http/headers.py
@@ -12,7 +12,8 @@ class Headers(Mapping):
         diesel_headers = DieselHeaders()
         diesel_headers.parse(headers)
         self._diesel_headers = diesel_headers
-        Mapping.__init__(self, **diesel_headers._headers)
+        Mapping.__init__(self)
+        self._dict.update(diesel_headers._headers)
 
     def to_http(self):
         """Return the headers as a string, formatted for an HTTP message.
