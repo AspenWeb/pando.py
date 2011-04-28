551cdabba71ea1ff715917e003552f7174d2b22a
Chad Whitacre <chad@zetaweb.com>
2011-04-28 16:40:20 -0400

    I added support for static JSON simplates.

--- aspen/__init__.py
+++ aspen/__init__.py
@@ -3,7 +3,10 @@ from aspen.http import Response
 try:                # Python 2.6+
     import json
 except ImportError: # Python 2.5-
-    import simplejson as json
+    try:
+        import simplejson as json
+    except ImportError:
+        json = None
 
 
 __version__ = "~~VERSION~~"
