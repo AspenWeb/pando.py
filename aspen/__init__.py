60d0230754075125079f6d42a932fe20f0856eff
Chad Whitacre <chad@zetaweb.com>
2012-03-14 01:47:09 -0700

    Added cheroot support.

--- aspen/__init__.py
+++ aspen/__init__.py
@@ -7,8 +7,8 @@ from aspen import json_ as json
 
 __version__ = "~~VERSION~~"
 WINDOWS = sys.platform[:3] == 'win'
-ENGINES = ['cherrypy', 'diesel', 'eventlet', 'gevent', 'pants', 'rocket', 
-           'tornado', 'twisted']
+ENGINES = ['cheroot', 'cherrypy', 'diesel', 'eventlet', 'gevent', 'pants', 
+           'rocket', 'tornado', 'twisted']
 
 
 def thrash():
