42a15f4d1b18c506351348e49a031adb4056ba31
Chad Whitacre <chad@zetaweb.com>
2012-04-17 02:25:01 -0400

    Trying to get ga on http://aspen.io/

--- doc/.aspen/configure-aspen.py
+++ doc/.aspen/configure-aspen.py
@@ -1,7 +1,10 @@
+import os
+
+from aspen.configuration import parse
 from aspen_io import opts, inbound
 
 
-opts['show_ga'] = False
+opts['show_ga'] = parse.yes_no(os.environ.get('ASPEN_IO_SHOW_GA', 'no'))
 opts['base'] = ''
 opts['version'] = open('../version.txt').read().strip('+')
 
