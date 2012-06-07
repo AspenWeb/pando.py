faeaf2ea9a10f7adb86bdc2e0f3d9f6cf1b8c4ff
Chad Whitacre <chad@zetaweb.com>
2012-06-07 15:57:28 -0400

    I forgot to add these tests a while ago

---
+++ tests/test_execution.py
@@ -0,0 +1,20 @@
+from aspen import execution
+from aspen.testing.fsfix import attach_teardown
+
+class Foo:
+    pass
+
+def test_startup_basically_works():
+    website = Foo()
+    website.changes_kill = True
+    website.root = 'foo'
+    website.network_engine = Foo()
+    website.network_engine.start_checking = lambda x: x
+    website.configuration_scripts = []
+    execution.install(website)
+    expected = set()
+    actual = execution.extras
+    assert actual == expected, actual
+
+
+attach_teardown(globals())
