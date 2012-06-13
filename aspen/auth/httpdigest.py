fc33b56b798b86afdd1415106a7812d8d3d0f0b6
Paul Jimenez <pj@place.org>
2012-06-13 00:45:10 -0500

    Fix pylint complaints and parameters to reduce warnings.

--- aspen/auth/httpdigest.py
+++ aspen/auth/httpdigest.py
@@ -14,26 +14,11 @@ except ImportError:
 
 class MalformedAuthenticationHeader(Exception): pass
 
-## wrapper bits that currently use web.py
-
-class DigestAuthWrapper(object):
-
-    def __init__(self, *args, **kw):
-        kw['HTTPProvider'] = WebPyHTTPProvider()
-        self.auth = Auth(*args, **kw)
-
-    def __call__(self, f):
-        def wrapper(*args, **kw):
-            authed, result = self.auth.authorized()
-            if authed:
-                return f(*args, **kw)
-            return result
-        return wrapper
-
-
 ## wrapper bits 
 
 class AspenHTTPProvider:
+    """An abstraction layer between the Auth object and
+    http-framework specific code."""
 
     def __init__(self, request):
         self.request = request
@@ -79,14 +64,13 @@ def inbound_responder(*args, **kw):
         password = users[username]
         return digestauth.digest(':'.join([username, realm, password]))
 
-    website.hooks.inbound_early.register(digestauth.inbound_responder(get_digest))
-
+    website.hooks.inbound_early.register(digestauth.inbound_responder(get_digest)) 
     """
-
     kwargs = kw.copy()
-    kwargs['HTTPProvider'] = AspenHTTPProvider
+    kwargs['http_provider'] = AspenHTTPProvider
     auth = Auth(*args, **kwargs)
     def _(request):
+        """generated hook function"""
         request.auth = AspenAuthWrapper(auth, request)
         authed, response = auth.authorized(request)
         if not authed:
@@ -97,17 +81,25 @@ def inbound_responder(*args, **kw):
 
 
 class AspenAuthWrapper(object):
+    """Convenience class to put on a request that
+       has a reference to the request its on so accessing
+       auth methods doesn't require repeating the request arg.
+    """
+
     def __init__(self, auth, request):
         self.auth = auth
         self.request = request
 
     def authorized(self):
+        """delegates to self.auth object""" 
         return self.auth.authorized(self.request)[0]
 
     def username(self):
+        """delegates to self.auth object""" 
         return self.auth.username(self.request)
 
     def logout(self):
+        """delegates to self.auth object""" 
         return self.auth.logout(self.request)
 
 
@@ -157,24 +149,24 @@ class Storage(dict):
 
 class Auth(object):
     """A decorator class implementing digest authentication (RFC 2617)"""
-    def __init__(self,  get_digest,  realm="Protected",  tolerateIE = True, redirect_url = '/newuser',  unauth_html = None,  nonce_skip = 0,  lockout_time = 20,  nonce_life = 180,  tries=3,  domain=[], HTTPProvider=None):
+    def __init__(self,  get_digest,  realm="Protected",  tolerate_ie = True, redirect_url = '/newuser',  unauth_html = None,  nonce_skip = 0,  lockout_time = 20,  nonce_life = 180,  tries=3,  domain=[], http_provider=None):
         """Creates a decorator specific to a particular web application. 
             get_digest: a function taking the arguments (username, realm), and returning digestauth.digest(username:realm:password), or
                             throwing KeyError if no such user
             realm: the authentication "realm"
-            tolerateIE: don't deny requests from Internet Explorer, even though it is standards uncompliant and kind of insecure
+            tolerate_ie: don't deny requests from Internet Explorer, even though it is standards uncompliant and kind of insecure
             redirect_url:  when user hits "cancel," they are redirected here
             unauth_html:  the HTML that is sent to the user and displayed if they hit cancel (default is a redirect page to redirect_url)
             nonce_skip: tolerate skips in the nonce count, only up to this amount (useful if CSS or JavaScript is being loaded unbeknownst to your code)
             lockout_time: number of seconds a user is locked out if they send a wrong password (tries) times
             nonce_life: number of seconds a nonce remains valid
             tries: number of tries a user gets to enter a correct password before the account is locked for lockout_time seconds
-            HTTPProvider: interface to HTTP protocol workings (see above code)
+            http_provider: interface to HTTP protocol workings (see above code)
         """
-        self.http_provider = HTTPProvider
+        self.http_provider = http_provider
         if self.http_provider is None:
-            raise Exception("no HTTPProvider provided")
-        self.get_digest,  self.realm,  self.tolerateIE  = (get_digest,  realm,  tolerateIE)
+            raise Exception("no http_provider provided")
+        self.get_digest,  self.realm,  self.tolerate_ie  = (get_digest,  realm,  tolerate_ie)
         self.lockout_time,  self.tries,  self.nonce_life,  self.domain = (lockout_time,  tries - 1,  nonce_life,  domain)
         self.unauth_html = unauth_html or self._default_401_html.replace("$redirecturl",  redirect_url)
         self.outstanding_nonces = NonceMemory()
@@ -209,7 +201,7 @@ class Auth(object):
         req_header_dict = parse_auth_header(request_header)
         if not self._directive_proper(http.user_agent(), req_header_dict, http.path_and_query()):
             # Something is wrong with the authentication header
-            if req_header_dict.get('opaque',self.opaque) != self.opaque:
+            if req_header_dict.get('opaque', self.opaque) != self.opaque:
                 # Didn't send back the correct "opaque;" probably, our server restarted.  Just send
                 # them another authentication header with the correct opaque field.
                 return False, self._send_401_unauth_response(http, "Incorrect opaque field.")
@@ -217,16 +209,16 @@ class Auth(object):
                 # Their header had a more fundamental problem.  Something is fishy.  Deny access.
                 return False, self._deny_bad_request(http, "Authorization Request Header does not conform to RFC 2617 section 3.2.2")
         # if user sent a "logout" nonce, make them type in the password again
-        if len(req_header_dict.nonce) != 34:
+        if len(req_header_dict['nonce']) != 34:
             return False, self._send_401_unauth_response(http, "Logged out.")
-        nonceReaction = self.outstanding_nonces.nonce_state(req_header_dict)
-        if nonceReaction == NonceMemory.NONCE_INVALID:
+        nonce_response = self.outstanding_nonces.nonce_state(req_header_dict)
+        if nonce_response == NonceMemory.NONCE_INVALID:
             # Client sent a nonce we've never heard of before
             return False, self._deny_bad_request(http)
-        if nonceReaction == NonceMemory.NONCE_OLD:
+        if nonce_response == NonceMemory.NONCE_OLD:
             # Client sent an old nonce.  Give the client a new one, and ask to authenticate again before continuing.
             return False, self._send_401_unauth_response(http, "Stale nonce. Try again.", stale=True)
-        username = req_header_dict.username
+        username = req_header_dict['username']
         status = self.user_status.get(username, (self.tries, 0))
         if status[0] < 1 and time.time() < status[1]:
             # User got the password wrong within the last (self.lockout_time) seconds
@@ -248,21 +240,22 @@ class Auth(object):
             return False, self._send_401_unauth_response(http, "Wrong password. One try burned.")
 
     def _log_incorrect_password(self,  username,  req_header_dict):
+        """Hook to log incorrrect password attempts"""
         pass  # Do your own logging here
 
-    def _directive_proper(self,  user_agent, req_header_dict, reqPath):
+    def _directive_proper(self,  user_agent, req_header_dict, req_path):
         """Verifies that the client's authentication header contained the required fields"""
-        for variable in ['username','realm','nonce','uri','response','cnonce','nc']:
+        for variable in ['username', 'realm', 'nonce', 'uri', 'response', 'cnonce', 'nc']:
             if variable not in req_header_dict:
                 return False
         # IE doesn't send "opaque" and does not include GET parameters in the Digest field
-        standardsUncompliant = self.tolerateIE and ("MSIE" in user_agent)
+        standards_uncompliant = self.tolerate_ie and ("MSIE" in user_agent)
         return req_header_dict['realm'] == self.realm \
-            and (standardsUncompliant or req_header_dict.get('opaque','') == self.opaque) \
+            and (standards_uncompliant or req_header_dict.get('opaque','') == self.opaque) \
             and len(req_header_dict['nc']) == 8 \
-            and (req_header_dict['uri'] == reqPath or (standardsUncompliant and "?" in reqPath and reqPath.startswith(req_header_dict['uri'])))
+            and (req_header_dict['uri'] == req_path or (standards_uncompliant and "?" in req_path and req_path.startswith(req_header_dict['uri'])))
 
-    def _request_digest_valid(self, req_header_dict, reqMethod):
+    def _request_digest_valid(self, req_header_dict, req_method):
         """Checks to see if the client's request properly authenticates"""
         # Ask the application for the hash of A1 corresponding to this username and realm
         try:
@@ -271,7 +264,7 @@ class Auth(object):
             # No such user
             return False
         qop = req_header_dict.get('qop','auth')
-        A2 = reqMethod + ':' + req_header_dict['uri']
+        A2 = req_method + ':' + req_header_dict['uri']
         # auth-int stuff would go here, but few browsers support it
         nonce = req_header_dict['nonce']
         # Calculate the response we should have received from the client
@@ -280,6 +273,7 @@ class Auth(object):
         return req_header_dict['response'] == correct_answer
 
     def _send_401_unauth_response(self, http, why_msg, stale=False):
+        """send a 401, optionally with a stale flag"""
         nonce = self.outstanding_nonces.get_new_nonce(self.nonce_life)
         challenge_list = [ "realm=" + quote_it(self.realm), 
                            'qop="auth"',
@@ -319,7 +313,7 @@ class Auth(object):
             req_header_dict = self._get_valid_auth_header(http)
         except MalformedAuthenticationHeader:
             return
-        if len(req_header_dict.nonce) == 34:
+        if len(req_header_dict['nonce']) == 34:
             # First time: send a 401 giving the user the fake "logout" nonce
             nonce = "%032x" % random.getrandbits(136)
             challenge_list = [ "realm=" + quote_it(self.realm), 
@@ -337,10 +331,10 @@ class Auth(object):
             req_header_dict = self._get_valid_auth_header(http)
         except MalformedAuthenticationHeader:
             return None
-        if len(req_header_dict.nonce) != 34:
+        if len(req_header_dict['nonce']) != 34:
             return None
-        nonceReaction = self.outstanding_nonces.nonce_state(req_header_dict)
-        if nonceReaction in [ 2, 3 ] :
+        nonce_response = self.outstanding_nonces.nonce_state(req_header_dict)
+        if nonce_response != NonceMemory.NONCE_VALID:
             # Client sent a nonce we've never heard of before
             # Client sent an old nonce.  Give the client a new one, and ask to authenticate again before continuing.
             return None
@@ -352,9 +346,9 @@ def digest(data):
     """Return a hex digest MD5 hash of the argument"""
     return md5(data).hexdigest()
 
-def quote_it(x):
+def quote_it(s):
     """Return the argument quoted, suitable for a quoted-string"""
-    return '"%s"' % (x.replace("\\","\\\\").replace('"','\\"'))
+    return '"%s"' % (s.replace("\\","\\\\").replace('"','\\"'))
 
 ## Code to parse the authentication header
 parse_auth_header_re = re.compile(r"""
@@ -363,18 +357,20 @@ parse_auth_header_re = re.compile(r"""
     (   (?P<var>[a-z]+)=(?P<value>.+?)(,|$)    )          # match variable=value,  (same as above, but no quotes)
     """,  re.VERBOSE | re.IGNORECASE )
 def parse_auth_header(header):
-    d = Storage()
+    """parse an authentication header into a dict"""
+    result = Storage()
     for m in parse_auth_header_re.finditer(header):
         g = m.groupdict()
         if g['varq'] and g['valueq']:
-            d[g['varq']] = g['valueq'].replace(r'\"',  '"')
+            result[g['varq']] = g['valueq'].replace(r'\"',  '"')
         elif g['var'] and g['value']:
-            d[g['var']] = g['value']
-        else:
-            return {}
-    return d
+            result[g['var']] = g['value']
+    return result
 
 class NonceMemory(dict):
+    """
+    A dict of in-use nonces, with a couple methods to create new nonces and get the state of a nonce
+    """
 
     NONCE_VALID = 1
     NONCE_INVALID = 2
@@ -397,7 +393,7 @@ class NonceMemory(dict):
     def nonce_state(self, req_header_dict):
         """ 1 = nonce valid, proceed; 2 = nonce totally invalid;  3 = nonce requires refreshing """
         nonce = req_header_dict.get('nonce', None)
-        exp_time, nCount = self.get(nonce, (0,0) )
+        exp_time, nCount = self.get(nonce, (0, 0) )
         if exp_time == 0:
             # Client sent some totally unknown nonce -- reject
             return self.NONCE_INVALID
