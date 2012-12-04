28a1c63c238d39b82c4fb6b88a7a370f8b819135
Paul Jimenez <pj@place.org>
2012-12-04 09:05:25 -0600

    replace smoke.sh with smoke target in Makefile

--- smoke.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh 
-mkdir foo
-cd foo
-echo "made directory ./foo/ and changed to it"
-virtualenv .aspen
-source .aspen/bin/activate
-pip install ../
-echo Greetings, program! > index.html
-aspen
