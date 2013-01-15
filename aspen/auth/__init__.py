eb5d5ebda18ee05a0c42ad52f5a2fbe0a4d8e3d1
Chad Whitacre <chad@zetaweb.com>
2013-01-15 16:02:15 -0500

    Add a require_authentication helper

--- aspen/auth/__init__.py
+++ aspen/auth/__init__.py
@@ -8,6 +8,7 @@ Currently:
 	* httpdigest - HTTP DIGEST Auth
 
 """
+from aspen import Response
 from aspen.utils import typecheck
 
 
@@ -22,3 +23,15 @@ class BaseUser(object):
         return self.token is None
 
 User = BaseUser
+
+
+def require_authentication(request):
+    """Given a request object, return None or raise Response(401).
+
+    Place this after hooks that will set request.context['user']. Use hook
+    filters to apply this to less than all requests. Use a 401.html file to
+    influence what happens when 401 is raised here.
+
+    """
+    if request.context['user'].ANON:
+        raise Response(401)
