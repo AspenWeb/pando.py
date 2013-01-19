a2309ec883b5c28929d142020213679c6e85623e
Chad Whitacre <chad@zetaweb.com>
2013-01-19 05:15:14 -0500

    Reformat for 80 chars

--- aspen/hooks/filters.py
+++ aspen/hooks/filters.py
@@ -2,10 +2,15 @@ import re
 
 
 def by_regex(hook, regex_tuples, default=True):
-    """A filter for hooks. regex_tuples is a list of (regex, filter?) where if the regex matches
-       the requested URI, then the hook is applied or not based on if filter? is True or False.
+    """A filter for hooks.
+
+    regex_tuples is a list of (regex, filter?) where if the regex matches the
+    requested URI, then the hook is applied or not based on if filter? is True
+    or False.
+
     """
-    regex_res = [ (re.compile(regex), disposition) for regex, disposition in regex_tuples.iteritems() ]
+    regex_res = [ (re.compile(regex), disposition) \
+                           for regex, disposition in regex_tuples.iteritems() ]
     def filtered_hook(request):
         for regex, disposition in regex_res:
             if regex.matches(request.line.uri):
@@ -20,8 +25,9 @@ def by_regex(hook, regex_tuples, default=True):
 def by_dict(hook, truthdict, default=True):
     """A filter for hooks.
 
-    truthdict is a mapping of URI -> filter? where if the requested URI is a key in the dict, then
-    the hook is applied based on the filter? value.
+    truthdict is a mapping of URI -> filter? where if the requested URI is a
+    key in the dict, then the hook is applied based on the filter? value.
+
     """
     def filtered_hook(request):
         do_hook = truthdict.get(request.line.uri, default)
