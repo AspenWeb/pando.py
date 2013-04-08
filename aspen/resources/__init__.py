e8781103e991014decd7f900f4b2a23ae4f3e7cc
Nathan West <nathan@mimedia.com>
2013-04-08 14:41:53 -0400

    Some fixes to resources/__init__.py
    SPLITTER and ESCAPED_SPLITTER now accept lines that don't end in \n
        for at the end of a file
    SPECLINE now requires whitespace to surround via

--- aspen/resources/__init__.py
+++ aspen/resources/__init__.py
@@ -35,22 +35,22 @@ import functools
 # Paginate methods.
 #=================
 
-SPLITTER = '^\[----+\](?P<header>.*?)\n'
-ESCAPED_SPLITTER = '^/(/*)(\[----+\].*?\n)'
-SPECLINE = '^(.*?)\s*(?:via\s*(.*?))?$'
+SPLITTER = '^\[----+\](?P<header>.*?)(\n|$)'
+ESCAPED_SPLITTER = '^/(/*)(\[----+\].*?(\n|$))'
+SPECLINE = '^(.*?)\s+(?:via\s+(.*?))?\s*$'
 
 SPLITTER = re.compile(SPLITTER, re.MULTILINE)
 ESCAPED_SPLITTER = re.compile(ESCAPED_SPLITTER, re.MULTILINE)
-SPECLINE = re.compile(SPECLINE, re.MULTILINE)
+SPECLINE = re.compile(SPECLINE)
 
 class Page(object):
     __slots__ = ('header', 'content', 'offset')
-    
+
     def __init__(self, content, header='', offset=0):
         self.content = content
         self.header = header
         self.offset = offset
-    
+
     @property
     def padded_content(self):
         return ('\n' * self.offset) + self.content
@@ -59,22 +59,22 @@ def split(raw):
     '''Pure split generator. This function defines the plain logic to split a
     string into a list of pages
     '''
-    
+
     current_index = 0
     line_offset = 0
     header = ''
-    
+
     for page_break in SPLITTER.finditer(raw):
         content = raw[current_index:page_break.start()]
         yield Page(content, header, line_offset)
         line_offset += content.count('\n') + 1
         header = page_break.group('header').strip()
         current_index = page_break.end()
-    
+
     # Yield final page. If no page dividers were found, this will be all of it
     content = raw[current_index:]
     yield Page(content, header, line_offset)
-        
+
 def escape(content):
     '''Pure escape method. This function defines the logic to properly convert
     escaped splitter patterns in a string
@@ -87,13 +87,13 @@ def split_and_escape(raw):
     for page in split(raw):
         page.content = escape(page.content)
         yield page
-        
+
 def parse_specline(header):
     '''Attempt to parse the header in a page returned from split(...) as a
     specline. Returns a tuple (content_type, renderer)
     '''
     return SPECLINE.match(header).groups('')
-    
+
 def can_split(raw, splitter=SPLITTER):
     '''Determine if a text block would be split by a splitter
     '''
