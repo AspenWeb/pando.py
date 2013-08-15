f409dc47eaafe7542fe6e549d15334702d2b85bf
Paul Jimenez <pj@place.org>
2013-08-15 11:25:38 -0400

    move extract_rfc2396_params to request.py
    refactor extract_rfc2396_params some so it fits in better

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -460,29 +460,36 @@ def test_intercept_socket_intercepts_transported():
 
 # URI pathsegment parameters
 
-def _extract_params(path):
-    return dispatcher.extract_rfc2396_params(path.lstrip('/').split('/'))
+def _extract_params(uri):
+#    return dispatcher.extract_rfc2396_params(path.lstrip('/').split('/'))
+    params = [ segment.params for segment in uri.path.segments ]
+    segments = [ unicode(segment) for segment in uri.path.segments ]
+    return ( segments, params )
 
 def test_extract_path_params_with_none():
     request = Request(uri="/foo/bar")
-    actual = _extract_params(request.line.uri.path.decoded)
+    actual = _extract_params(request.line.uri)
     expected = (['foo', 'bar'], [{}, {}]) 
     assert actual == expected
 
 def test_extract_path_params_simple():
     request = Request(uri="/foo;a=1;b=2;c/bar;a=2;b=1")
-    actual = _extract_params(request.line.uri.path.decoded)
+    actual = _extract_params(request.line.uri)
     expected = (['foo', 'bar'], [{'a':'1', 'b':'2', 'c':[]}, {'a':'2', 'b':'1'}]) 
     assert actual == expected
 
 def test_extract_path_params_complex():
     request = Request(uri="/foo;a=1;b=2,3;c/bar;a=2,ab;b=1")
-    actual = _extract_params(request.line.uri.path.decoded)
+    actual = _extract_params(request.line.uri)
     expected = (['foo', 'bar'], [{'a':'1', 'b':['2', '3'], 'c':[]}, {'a':[ '2', 'ab' ], 'b':'1'}]) 
     assert actual == expected
 
-
-
+def test_path_params_api():
+    request = Request(uri="/foo;a=1;b=2,3;c/bar;a=2,ab;b=1")
+    segs, params = (['foo', 'bar'], [{'a':'1', 'b':['2', '3'], 'c':[]}, {'a':[ '2', 'ab' ], 'b':'1'}]) 
+    assert request.line.uri.path.segments == segs, request.line.uri.path.segments
+    assert request.line.uri.path.segments[0].params == params[0]
+    assert request.line.uri.path.segments[1].params == params[1]
 
 # mongs
 # =====
