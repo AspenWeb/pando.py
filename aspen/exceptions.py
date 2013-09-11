d4ac378e750dc3dca5a2130b996b0232932b1042
Chad Whitacre <chad@zetaweb.com>
2013-09-10 22:52:42 -0400

    Add a failing test for CRLF injection
    http://www.acunetix.com/websitesecurity/crlf-injection/

--- aspen/exceptions.py
+++ aspen/exceptions.py
@@ -6,3 +6,8 @@ class LoadError(Exception):
     """Represent a problem loading a resource.
     """
     # Define this here to avoid import issues when json doesn't exist.
+
+
+class CRLFInjection(Exception):
+    def __str__(self):
+        return "Possible CRLF injection detected."
