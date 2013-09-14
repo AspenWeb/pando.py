5930faa1bf5fde83b0c5dfad35c400f1c6eca052
Chad Whitacre <chad@zetaweb.com>
2013-09-13 22:11:38 -0400

    Here's a failing test for Path().parts

--- tests/test_request_line.py
+++ tests/test_request_line.py
@@ -368,6 +368,11 @@ def test_path_doesnt_unquote_plus():
     path = Path("/+%2B.html")
     assert path.decoded == u"/++.html", path.decoded
 
+def test_path_has_parts():
+    path = Path("/foo/bar.html")
+    assert path.parts == ['foo', 'bar.html']
+
+
 # Path params
 # ===========
 
@@ -380,24 +385,24 @@ def _extract_params(uri):
 def test_extract_path_params_with_none():
     request = Request(uri="/foo/bar")
     actual = _extract_params(request.line.uri)
-    expected = (['foo', 'bar'], [{}, {}]) 
+    expected = (['foo', 'bar'], [{}, {}])
     assert actual == expected, actual
 
 def test_extract_path_params_simple():
     request = Request(uri="/foo;a=1;b=2;c/bar;a=2;b=1")
     actual = _extract_params(request.line.uri)
-    expected = (['foo', 'bar'], [{'a':['1'], 'b':['2'], 'c':['']}, {'a':['2'], 'b':['1']}]) 
+    expected = (['foo', 'bar'], [{'a':['1'], 'b':['2'], 'c':['']}, {'a':['2'], 'b':['1']}])
     assert actual == expected, actual
 
 def test_extract_path_params_complex():
     request = Request(uri="/foo;a=1;b=2,3;c;a=2;b=4/bar;a=2,ab;b=1")
     actual = _extract_params(request.line.uri)
-    expected = (['foo', 'bar'], [{'a':['1','2'], 'b':['2,3', '4'], 'c':['']}, {'a':[ '2,ab' ], 'b':['1']}]) 
+    expected = (['foo', 'bar'], [{'a':['1','2'], 'b':['2,3', '4'], 'c':['']}, {'a':[ '2,ab' ], 'b':['1']}])
     assert actual == expected, actual
 
 def test_path_params_api():
     request = Request(uri="/foo;a=1;b=2;b=3;c/bar;a=2,ab;b=1")
-    segs, params = (['foo', 'bar'], [{'a':['1'], 'b':['2', '3'], 'c':['']}, {'a':[ '2,ab' ], 'b':['1']}]) 
+    segs, params = (['foo', 'bar'], [{'a':['1'], 'b':['2', '3'], 'c':['']}, {'a':[ '2,ab' ], 'b':['1']}])
     assert request.line.uri.path.parts == segs, request.line.uri.path.parts
     assert request.line.uri.path.parts[0].params == params[0]
     assert request.line.uri.path.parts[1].params == params[1]
