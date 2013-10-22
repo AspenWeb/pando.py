2c97bf6869b97e1aae48fc08f7ead4cf17cddd63
Chad Whitacre <chad@zetaweb.com>
2013-10-22 11:54:40 -0400

    Move pytest fixture into the aspen library
    This way it can be imported and used by downstream projects.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,12 @@
 import pytest
 from aspen.testing import fsfix
-from aspen.testing.filesystem_fixture import FilesystemFixture
 from aspen.testing.fsfix import teardown
-from aspen.testing.harness import Harness
-from aspen.website import Website
+from aspen.testing.pytest_fixtures import harness
 
 
 @pytest.yield_fixture
 def mk():
     yield fsfix.mk
-
-@pytest.yield_fixture
-def harness():
-    www = FilesystemFixture()
-    project = FilesystemFixture()
-    website = Website(['--www_root', www.root, '--project_root', project.root])
-    yield Harness(website, www, project)
 
 def pytest_runtest_teardown():
     teardown()
