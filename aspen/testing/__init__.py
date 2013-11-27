415bd2d53610f268ca824b6be0866936d00f8fdb
Chad Whitacre <chad@zetaweb.com>
2013-11-27 16:33:27 -0500

    Fix test suite
    I moved the Harness into __init__.py and split it into two: a Harness
    base class to be shared with downstream apps, and a _Harness class to be
    used in Aspen's own test suite.

--- aspen/testing/__init__.py
+++ aspen/testing/__init__.py
@@ -5,7 +5,18 @@ from __future__ import unicode_literals
 
 import os
 import sys
+from collections import namedtuple
+from Cookie import SimpleCookie
+from StringIO import StringIO
+
 from aspen import resources, sockets
+from aspen.http.request import Request
+from aspen.network_engines import ThreadedBuffer
+from aspen.server import Server
+from aspen.sockets.channel import Channel
+from aspen.sockets.socket import Socket
+from aspen.sockets.transport import XHRPollingTransport
+from aspen.testing.filesystem_fixture import FilesystemFixture
 
 
 CWD = os.getcwd()
@@ -31,3 +42,253 @@ def teardown():
     aspen.execution.clear_changes()
 
 teardown() # start clean
+
+
+BOUNDARY = b'BoUnDaRyStRiNg'
+MULTIPART_CONTENT = b'multipart/form-data; boundary=%s' % BOUNDARY
+
+
+def encode_multipart(boundary, data):
+    """
+    Encodes multipart POST data from a dictionary of form values.
+
+    Borrowed from Django
+    The key will be used as the form data name; the value will be transmitted
+    as content. If the value is a file, the contents of the file will be sent
+    as an application/octet-stream; otherwise, str(value) will be sent.
+    """
+    lines = []
+
+    for (key, value) in data.items():
+        lines.extend([
+            b'--' + boundary,
+            b'Content-Disposition: form-data; name="%s"' % str(key),
+            b'',
+            str(value)
+        ])
+
+    lines.extend([
+        b'--' + boundary + b'--',
+        b'',
+    ])
+    return b'\r\n'.join(lines)
+
+
+class Harness(object):
+    """
+    The Aspen testing harness.
+
+    Used in tests to emulate ``GET`` and ``POST`` requests by sending them
+    into a ``Website`` instance's ``respond`` method.
+
+    Aspen does not define any User data structures or modules. If you want to
+    do anything with users/sessions etc in your tests it is expected that you
+    will subclass this class and add a ``add_cookie_info`` method.
+
+    For example, in gittip a suitable subclass might be::
+
+        class GittipTestClient(TestClient):
+
+            def add_cookie_info(self, request, cookie_info):
+                if cookie_info:
+                    user = cookie_info.get('user')
+                    if user is not None:
+                        user = User.from_id(user)
+                        # Note that Cookie needs a bytestring.
+                        request.headers.cookie['session'] = user.session_token
+
+    Example usage in a test::
+
+        def test_api_handles_posts():
+            client = TestClient(website)
+
+            # We need to get ourselves a token!
+            response = client.get('/')
+            csrf_token = response.request.context['csrf_token']
+
+            # Then, add a $1.50 and $3.00 tip
+            response = client.post("/test_tippee1/tip.json",
+                                {'amount': "1.00", 'csrf_token': csrf_token},
+                                cookie_info={'user': 'test_tipper'})
+
+            # Confirm we get back the right amounts in the JSON body.
+            first_data = json.loads(response.body)
+            assert_equal(first_data['amount'], "1.00")
+    """
+
+    def __init__(self, www_root=None, project_root=None, argv=None):
+        self.fs = namedtuple('fs', 'www project')
+        self.fs.www = FilesystemFixture(root=www_root)
+        self.fs.project = FilesystemFixture(root=project_root)
+        self.argv = [] if argv is None else argv
+        self.cookies = SimpleCookie()
+        self.short_circuit = True
+        self._website = None
+
+
+    @property
+    def website(self):
+        if self._website is None:
+            argv = [ '--www_root', self.fs.www.root
+                   , '--project_root', self.fs.project.root
+                    ] + self.argv
+            website = Server(argv).get_website()
+            website.algorithm.short_circuit = self.short_circuit
+            self._website = website
+        return self._website
+
+
+    # HTTP Methods
+    # ============
+
+    def GET(self, path='/', cookie_info=None, run_through=None, want='response', **kw):
+        environ = self.build_wsgi_environ(path, "GET", **kw)
+        return self._perform_request(environ, cookie_info, run_through, want)
+
+
+    def POST(self, path='/', data=None, content_type=MULTIPART_CONTENT, cookie_info=None,
+            run_through=None, want='response', **kw):
+        """Perform a dummy POST request against the test website.
+
+        :param path:
+            The url to perform the virutal-POST to.
+
+        :param data:
+            A dictionary or list of tuples to be encoded before being POSTed.
+
+        Any additional parameters will be sent as headers. NOTE that in Aspen
+        (request.py make_franken_headers) only headers beginning with ``HTTP``
+        are included in the request - and those are changed to no longer
+        include ``HTTP``. There are currently 2 exceptions to this:
+        ``'CONTENT_TYPE'``, ``'CONTENT_LENGTH'`` which are explicitly checked
+        for.
+        """
+        post_data = data if data is not None else {}
+
+        if content_type is MULTIPART_CONTENT:
+            post_data = encode_multipart(BOUNDARY, data)
+
+        environ = self.build_wsgi_environ( path
+                                         , "POST"
+                                         , post_data
+                                         , CONTENT_TYPE=str(content_type)
+                                         , **kw
+                                          )
+        return self._perform_request(environ, cookie_info, run_through, want)
+
+
+    # Helpers
+    # =======
+
+    def build_wsgi_environ(self, path, method="GET", body=None, **kw):
+        environ = {}
+        environ['PATH_INFO'] = path
+        environ['REMOTE_ADDR'] = b'0.0.0.0'
+        environ['REQUEST_METHOD'] = b'GET'
+        environ['SERVER_PROTOCOL'] = b'HTTP/1.1'
+        environ['HTTP_HOST'] = b'localhost'
+        environ['REQUEST_METHOD'] = method
+        environ['wsgi.input'] = StringIO(body)
+        environ['HTTP_COOKIE'] = self.cookies.output(header='', sep='; ')
+        environ.update(kw)
+        return environ
+
+
+    def _perform_request(self, environ, cookie_info, run_through, want):
+        state = self.website.respond(environ, _run_through=run_through)
+
+        response = state.get('response')
+        if response is not None:
+            if response.headers.cookie:
+                self.cookies.update(response.headers.cookie)
+
+        attr_path = want.split('.')
+        base = attr_path[0]
+        attr_path = attr_path[1:]
+
+        out = state[base]
+        for name in attr_path:
+            out = getattr(out, name)
+
+        return out
+
+
+class _Harness(Harness):
+    """A subclass of the test client to be used in the Aspen test suite.
+    """
+
+    def teardown(self):
+        self.fs.www.remove()
+        self.fs.project.remove()
+
+
+    # Simple API
+    # ==========
+
+    def simple(self, contents='Greetings, program!', filepath='index.html.spt', uripath=None,
+            run_through=None, want='response', argv=None, **kw):
+        """A helper to create a file and hit it through our machinery.
+        """
+        if filepath is not None:
+            self.fs.www.mk((filepath, contents))
+        if argv is not None:
+            self.argv = argv
+
+        if uripath is None:
+            if filepath is None:
+                uripath = '/'
+            else:
+                uripath = '/' + filepath
+                if uripath.endswith('.spt'):
+                    uripath = uripath[:-len('.spt')]
+                for indexname in self.website.indices:
+                    if uripath.endswith(indexname):
+                        uripath = uripath[:-len(indexname)]
+                        break
+
+        return self.GET(uripath, run_through=run_through, want=want, **kw)
+
+    def make_request(self, *a, **kw):
+        kw['run_through'] = 'dispatch_request_to_filesystem'
+        kw['want'] = 'request'
+        return self.simple(*a, **kw)
+
+
+    # Sockets
+    # =======
+
+    def make_transport(self, content='', state=0):
+        self.fs.www.mk(('echo.sock.spt', content))
+        socket = self.make_socket()
+        transport = XHRPollingTransport(socket)
+        transport.timeout = 0.05 # for testing, could screw up the test
+        if state == 1:
+            transport.respond(Request(uri='/echo.sock'))
+        return transport
+
+    def make_socket_request(self, filename='echo.sock.spt'):
+        request = Request(uri='/echo.sock')
+        request.website = self.website
+        request.fs = self.fs.www.resolve(filename)
+        return request
+
+    def make_socket(self, filename='echo.sock.spt', channel=None):
+        request = self.make_socket_request(filename='echo.sock.spt')
+        if channel is None:
+            channel = Channel(request.line.uri.path.raw, ThreadedBuffer)
+        socket = Socket(request, channel)
+        return socket
+
+    def SocketInThread(client):
+
+        class _SocketInThread(object):
+
+            def __enter__(self, filename='echo.sock.spt'):
+                self.socket = client.make_socket(filename)
+                self.socket.loop.start()
+                return self.socket
+
+            def __exit__(self, *a):
+                self.socket.loop.stop()
+
+        return _SocketInThread()
