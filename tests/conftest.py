9c068608906aeccdba185cd05c3546b17624b1fe
Chad Whitacre <chad@zetaweb.com>
2014-01-04 22:13:26 -0500

    Kill module scrubber
    This is mucking with global module state (or something?) in a way that
    is effing up tests over in aspen-python-plugins. The aspen test suite
    runs without it, so we're nuking it.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 from aspen.testing.harness import teardown
-from aspen.testing.pytest_fixtures import client, harness, fs, module_scrubber
+from aspen.testing.pytest_fixtures import client, harness, fs
 from aspen.testing.pytest_fixtures import sys_path, sys_path_scrubber
 
 
