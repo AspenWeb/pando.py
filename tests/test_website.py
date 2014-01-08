15f33009eefe28723c8afa4d727a32d363892efd
Paul Jimenez <pj@place.org>
2014-01-07 21:17:15 -0500

    Testcase for #279: raising a Response() doesn't let you set the body

--- tests/test_website.py
+++ tests/test_website.py
@@ -74,6 +74,17 @@ raise Response(420)
     assert response.code == 420
     assert response.body == 'Told ya.'
 
+def test_nice_error_response_can_be_overriden_by_simplate(harness):
+    harness.fs.project.mk(('error.html', 'Told ya.'))
+    harness.fs.www.mk(('index.html.spt', """
+from aspen import Response
+[---]
+raise Response(420, "Different error")
+[---]"""))
+    response = harness.client.GET(raise_immediately=False)
+    assert response.code == 420
+    assert response.body == 'Different error'
+
 def test_nice_error_response_can_come_from_user_420_html(harness):
     harness.fs.project.mk(('420.html.spt', """
 msg = "Enhance your calm." if response.code == 420 else "Ok."
