d44991bbd5fe8e05e8655e4ad3c6d107070b05de
Matthew Iversen <teh.ivo@gmail.com>
2014-02-10 14:58:24 +1100

    Update virtualenv.py

--- vendor/virtualenv-1.11.2.py
+++ vendor/virtualenv-1.11.2.py
@@ -1,4 +1,4 @@
 # The history of this repository has been rewritten to erase the vendor/ directory
 # Below is the md5sum and size of the file that was in the original commit
-1a74b6a68077308521bb47569d6beb38
-101937
+6044ac9d29fc57cb3d52e8cb9bf3ec2d
+98477
