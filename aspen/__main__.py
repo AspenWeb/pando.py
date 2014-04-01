eeb4b62d130fda0ed70d7e9a65b35deaa5cfbaf5
Paul Jimenez <pj@place.org>
2014-04-01 10:28:52 -0400

    Add a main-module entrypoint

---
+++ aspen/__main__.py
@@ -0,0 +1,26 @@
+"""
+python -m aspen
+===============
+
+Aspen ships with a server (wsgiref.simple_server) that is
+suitable for development and testing.  It can be invoked via:
+
+    python -m aspen
+
+though even for development you'll likely want to specify a
+project root, so a more likely incantation is:
+
+    ASPEN_PROJECT_ROOT=/path/to/wherever python -m aspen
+
+For production deployment, you should probably deploy using 
+a higher performance WSGI server like Gunicorn, uwsgi, Spawning, 
+or the like.
+"""
+
+from aspen.website import Website                                                                           
+from wsgiref.simple_server import make_server                                                               
+
+
+
+if __name__ == '__main__':
+    make_server('0.0.0.0', 8080, Website()).serve_forever()
