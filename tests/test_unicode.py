64606d1a51836232a094ec166a39b3137b13311d
Chad Whitacre <chad@zetaweb.com>
2014-04-30 16:48:06 -0400

    Munge the coding line instead of removing it
    We want to keep line numbers the same, but Python complains if we have a
    coding line in a unicode that we try to exec. Includes tests.

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -4,6 +4,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from aspen.resources import decode_raw
+from pytest import raises
+
 
 def test_utf8(harness):
     expected = unichr(1758)
@@ -27,3 +30,67 @@ def test_utf8_char(harness):
         %(text)s
     """).body.strip()
     assert actual == expected
+
+
+# decode_raw
+
+def test_decode_raw_can_take_encoding_from_first_line():
+    actual = decode_raw(b"""\
+    # -*- coding: utf8 -*-
+    text = u'א'
+    """)
+    expected = """\
+    # encoding set to utf8
+    text = u'א'
+    """
+    assert actual == expected
+
+def test_decode_raw_can_take_encoding_from_second_line():
+    actual = decode_raw(b"""\
+    #!/blah/blah
+    # -*- coding: utf8 -*-
+    text = u'א'
+    """)
+    expected = """\
+    #!/blah/blah
+    # encoding set to utf8
+    text = u'א'
+    """
+    assert actual == expected
+
+def test_decode_raw_can_take_encoding_from_various_line_formats():
+    formats = [ b'-*- coding: utf8 -*-'
+              , b'-*- encoding: utf8 -*-'
+              , b'coding: utf8'
+              , b'  coding: utf8'
+              , b'\tencoding: utf8'
+              , b'\t flubcoding=utf8'
+               ]
+    for fmt in formats:
+        def test():
+            actual = decode_raw(b"""\
+            # {0}
+            text = u'א'
+            """.format(fmt))
+            expected = """\
+            # encoding set to utf8
+            text = u'א'
+            """
+            assert actual == expected
+        yield test
+
+def test_decode_raw_cant_take_encoding_from_bad_line_formats():
+    formats = [ b'-*- coding : utf8 -*-'
+              , b'foo = 0 -*- encoding: utf8 -*-'
+              , b'  coding : utf8'
+              , b'encoding : utf8'
+              , b'  flubcoding =utf8'
+               ]
+    for fmt in formats:
+        def test():
+            raw = b"""\
+            # {0}
+            text = u'א'
+            """.format(fmt)
+            raises(UnicodeDecodeError, decode_raw, raw)
+        yield test
