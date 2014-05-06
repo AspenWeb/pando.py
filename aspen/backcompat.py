1324f91c4b417fcd873c4f74d6692132652ca602
Paul Jimenez <pj@place.org>
2014-05-06 11:47:43 -0400

    HTML-escape the tracebacks in our error template
    Fix #68

--- aspen/backcompat.py
+++ aspen/backcompat.py
@@ -33,6 +33,15 @@ try:                # 2
 except ImportError: # 3
     from http.cookies import CookieError, SimpleCookie
 
+try:                # 3
+    from html import escape as html_escape
+except ImportError: # 2
+    from cgi import escape as cgi_escape
+    def html_escape(*args,**kwargs):
+        # make the defaults match the py3 defaults
+        kwargs['quote'] = kwargs.get('quote', True)
+        return cgi_escape(*args,**kwargs)
+
 
 def _namedtuple(typename, field_names, verbose=False, rename=False):
     """Returns a new subclass of tuple with named fields.
