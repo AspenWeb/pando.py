21759edaf9f2b9ef2452713f0921cfe4dc8280dc
Chad Whitacre <chad@zetaweb.com>
2014-07-07 15:51:32 -0400

    Failing test for test client body; #360

---
+++ tests/test_test_client.py
@@ -0,0 +1,9 @@
+
+def test_test_client_handles_body(harness):
+    harness.fs.www.mk(('foo.spt', '''
+    [---]
+    bar = body['bar']
+    [---] text/html via stdlib_format
+    {bar}
+    '''))
+    harness.client.POST('/foo', data={'bar': '42'})
