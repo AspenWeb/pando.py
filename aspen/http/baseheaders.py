eff4a2e3827c9255bd9df2810101ed709722a38a
Paul Jimenez <pj@place.org>
2014-07-30 09:30:02 -0400

    Stop accepting bad headers; fixes #369

--- aspen/http/baseheaders.py
+++ aspen/http/baseheaders.py
@@ -26,14 +26,22 @@ class BaseHeaders(CaseInsensitiveMapping):
         """
         typecheck(d, (dict, str))
         if isinstance(d, str):
+            from aspen.exceptions import MalformedHeader
+
             def genheaders():
                 for line in d.splitlines():
+                    if b':' not in line:
+                        # no colon separator in header
+                        raise MalformedHeader(line)
                     k, v = line.split(b':', 1)
-                    yield k.strip(), v.strip()
+                    if k != k.strip():
+                        # disallowed leading or trailing whitspace
+                        # (per http://tools.ietf.org/html/rfc7230#section-3.2.4)
+                        raise MalformedHeader(line)
+                    yield k, v.strip()
         else:
             genheaders = d.iteritems
         CaseInsensitiveMapping.__init__(self, genheaders)
-
 
         # Cookie
         # ======
@@ -42,7 +50,7 @@ class BaseHeaders(CaseInsensitiveMapping):
         try:
             self.cookie.load(self.get('Cookie', b''))
         except CookieError:
-            pass # XXX really?
+            pass  # XXX really?
 
 
     def __setitem__(self, name, value):
