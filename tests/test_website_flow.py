5da445a0a4b6a535de38a661453dc040c2b078f4
Chad Whitacre <chad@zetaweb.com>
2014-10-13 10:22:50 -0400

    Failing test for request.context

--- tests/test_website_flow.py
+++ tests/test_website_flow.py
@@ -12,3 +12,14 @@ def test_website_can_respond(harness):
 def test_404_comes_out_404(harness):
     harness.fs.project.mk(('404.spt', '[---]\n[---] text/plain\nEep!'))
     assert harness.client.GET(raise_immediately=False).code == 404
+
+
+def test_request_context_can_be_populated_by_user(harness):
+    harness.fs.www.mk(('index.html.spt', '%(foo)s'))
+    harness.fs.project.mk(('configure-aspen.py', """\
+def add_foo_to_context(request):
+    request.context['foo'] = 'bar'
+
+website.algorithm.insert_after('parse_environ_into_request', add_foo_to_context)
+"""))
+    assert harness.client.GET().body == 'bar'
