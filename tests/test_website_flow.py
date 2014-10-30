e867e982a600ddce841c80d369205ca50f955f3c
Chad Whitacre <chad@zetaweb.com>
2014-10-30 12:02:40 -0400

    Failing test for #393

--- tests/test_website_flow.py
+++ tests/test_website_flow.py
@@ -4,6 +4,10 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 
+from aspen.http.request import Request
+from aspen.http.response import Response
+
+
 def test_website_can_respond(harness):
     harness.fs.www.mk(('index.html.spt', 'Greetings, program!'))
     assert harness.client.GET().body == 'Greetings, program!'
@@ -23,3 +27,14 @@ def add_foo_to_context(request):
 website.algorithm.insert_after('parse_environ_into_request', add_foo_to_context)
 """))
     assert harness.client.GET().body == 'bar'
+
+
+def test_early_failures_dont_break_everything(harness):
+    old_from_wsgi = Request.from_wsgi
+    def broken_from_wsgi(*a, **kw):
+        raise Response(400)
+    try:
+        Request.from_wsgi = classmethod(broken_from_wsgi)
+        assert harness.client.GET("/", raise_immediately=False).code == 400
+    finally:
+        Request.from_wsgi = old_from_wsgi
