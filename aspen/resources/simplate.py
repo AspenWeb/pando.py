8364fb13f4958896191a5d900affef304bd1649b
Chad Whitacre <chad@zetaweb.com>
2014-12-08 19:56:00 -0500

    Naively cram Negotiated and Rendered into Simplate

--- aspen/resources/simplate.py
+++ aspen/resources/simplate.py
@@ -7,9 +7,17 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from aspen import Response
-from aspen.resources.pagination import split_and_escape, Page
+import re
+import sys
+
+import mimeparse
+from aspen import Response, log
+from aspen.resources.pagination import split_and_escape, Page, parse_specline
 from aspen.resources.resource import Resource
+
+
+renderer_re = re.compile(r'[a-z0-9.-_]+$')
+media_type_re = re.compile(r'[A-Za-z0-9.+*-]+/[A-Za-z0-9.+*-]+$')
 
 
 class StringDefaultingList(list):
@@ -187,20 +195,179 @@ class DynamicResource(Resource):
         #Note that range(x) returns an empty list if x < 1
         pages[0:0] = (Page('') for _ in range(num_extra_pages))
 
-    # Hooks
-    # =====
 
-    def compile_page(self, *a):
-        """Given a page, return an object.
-        """
-        raise NotImplementedError
+    # Negotiated
+    # ==========
 
-    def process_raised_response(self, response):
-        """Given a response object, mutate it as needed.
+    min_pages = 3
+    max_pages = None
+
+
+    def __init__(self, *a, **kw):
+        self.renderers = {}         # mapping of media type to render function
+        self.available_types = []   # ordered sequence of media types
+        DynamicResource.__init__(self, *a, **kw)
+
+
+    def compile_page(self, page):
+        """Given a bytestring, return a (renderer, media type) pair.
         """
-        pass
+        make_renderer, media_type = self._parse_specline(page.header)
+        renderer = make_renderer(self.fs, page.content)
+        if media_type in self.renderers:
+            raise SyntaxError("Two content pages defined for %s." % media_type)
+
+        # update internal data structures
+        self.renderers[media_type] = renderer
+
+        self.available_types.append(media_type)
+
+        return (renderer, media_type)  # back to parent class
 
     def get_response(self, context):
-        """Given a context dictionary, return a Response object.
+        """Given a context dict, return a response object.
         """
-        raise NotImplementedError
+        request = context['request']
+        dispatch_result = context['dispatch_result']
+
+        # find an Accept header
+        accept = dispatch_result.extra.get('accept', None)
+        if accept is not None:      # indirect negotiation
+            failure = Response(404)
+        else:                       # direct negotiation
+            accept = request.headers.get('Accept', None)
+            msg = "The following media types are available: %s."
+            msg %= ', '.join(self.available_types)
+            failure = Response(406, msg.encode('US-ASCII'))
+
+        # negotiate or punt
+        render, media_type = self.pages[2]  # default to first content page
+        if accept is not None:
+            try:
+                media_type = mimeparse.best_match(self.available_types, accept)
+            except:
+                # exception means don't override the defaults
+                log("Problem with mimeparse.best_match(%r, %r): %r " % (self.available_types, accept, sys.exc_info()))
+            else:
+                if media_type == '':    # breakdown in negotiations
+                    raise failure
+                del failure
+                render = self.renderers[media_type] # KeyError is a bug
+
+        response = context['response']
+        response.body = render(context)
+        if 'Content-Type' not in response.headers:
+            response.headers['Content-Type'] = media_type
+            if media_type.startswith('text/'):
+                charset = response.charset
+                if charset is not None:
+                    response.headers['Content-Type'] += '; charset=' + charset
+
+        return response
+
+    def _parse_specline(self, specline):
+        """Given a bytestring, return a two-tuple.
+
+        The incoming string is expected to be of the form:
+
+            media_type via renderer
+
+        The renderer is optional. It will be computed based on media type if
+        absent. The return two-tuple contains a render function and a media
+        type (as unicode). SyntaxError is raised if there aren't one or two
+        parts or if either of the parts is malformed. If only one part is
+        passed in it's interpreted as a media type.
+
+        """
+        # Parse into parts
+        parts = parse_specline(specline)
+
+        #Assign parts
+        media_type, renderer = parts
+        if renderer == '':
+            renderer = self.website.default_renderers_by_media_type[media_type]
+
+        # Validate media type.
+        if media_type_re.match(media_type) is None:
+            msg = ("Malformed media type %s in specline %s. It must match "
+                   "%s.")
+            msg %= (media_type, specline, media_type_re.pattern)
+            raise SyntaxError(msg)
+
+        # Hydrate and validate renderer.
+        make_renderer = self._get_renderer_factory(media_type, renderer)
+
+        # Return.
+        return (make_renderer, media_type)
+
+
+    def _get_renderer_factory(self, media_type, renderer):
+        """Given two bytestrings, return a renderer factory or None.
+        """
+        if renderer_re.match(renderer) is None:
+            possible =', '.join(sorted(self.website.renderer_factories.keys()))
+            msg = ("Malformed renderer %s. It must match %s. Possible "
+                   "renderers (might need third-party libs): %s.")
+            raise SyntaxError(msg % (renderer, renderer_re.pattern, possible))
+
+        renderer = renderer.decode('US-ASCII')
+
+        factories = self.website.renderer_factories
+        make_renderer = factories.get(renderer, None)
+        if isinstance(make_renderer, ImportError):
+            raise make_renderer
+        elif make_renderer is None:
+            possible = []
+            want_legend = False
+            for k, v in sorted(factories.iteritems()):
+                if isinstance(v, ImportError):
+                    k = '*' + k
+                    want_legend = True
+                possible.append(k)
+            possible = ', '.join(possible)
+            if want_legend:
+                legend = " (starred are missing third-party libraries)"
+            else:
+                legend = ''
+            raise ValueError("Unknown renderer for %s: %s. Possible "
+                             "renderers%s: %s."
+                             % (media_type, renderer, legend, possible))
+        return make_renderer
+
+
+
+    # Rendered
+    # ========
+
+    min_pages = 1
+    max_pages = 4
+
+
+    def parse_into_pages(self, raw):
+        """Extend to insert page one if needed.
+        """
+        pages = self.parse_into_pages(self, raw)
+        self._prepend_empty_pages(pages, 3)
+        return pages
+
+
+    def _parse_specline(self, specline):
+        """Override to simplify.
+
+        Rendered resources have a simpler specline than negotiated resources.
+        They don't have a media type, and the renderer is optional.
+
+        """
+        #parse into parts.
+        parts = parse_specline(specline)
+
+        #Assign parts, discard media type
+        renderer = parts[1]
+        media_type = self.media_type
+        if not renderer:
+            renderer = self.website.default_renderers_by_media_type[media_type]
+
+        #Hydrate and validate renderer
+        make_renderer = self._get_renderer_factory(media_type, renderer)
+
+        return (make_renderer, media_type)
