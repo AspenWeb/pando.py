663c96fdfc54a0ae1feb42a46502599f85102f5d
Chad Whitacre <chad@zetaweb.com>
2015-01-25 14:11:36 -0500

    Implement request.qs alias

--- aspen/http/request.py
+++ aspen/http/request.py
@@ -230,6 +230,7 @@ class Request(str):
         # set up aliases
         obj.cookie = obj.headers.cookie
         obj.path = obj.line.uri.path
+        obj.qs = obj.line.uri.querystring
 
         return obj
 
--- aspen/resources/simplate.py
+++ aspen/resources/simplate.py
@@ -79,7 +79,6 @@ class Simplate(Resource):
         context.update(request.context)
         context.update({
             'website': None,
-            'qs': request.line.uri.querystring,
             'channel': None
         })
         # http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html
