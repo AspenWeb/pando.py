0a7b88df526016672e18608339524cdb527e5928
Chad Whitacre <chad@zetaweb.com>
2015-06-29 14:30:36 -0400

    Fix ImportError in `python -m aspen`

--- aspen/__main__.py
+++ aspen/__main__.py
@@ -21,8 +21,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from aspen import serve, Website
+from aspen import serve, website
 
 if __name__ == '__main__':
-    website = Website()
-    serve(website)
+    serve(website.Website())
