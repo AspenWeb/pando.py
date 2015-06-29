4cb0e4d31bd22335e6f357d826a59030c5698460
Chad Whitacre <chad@zetaweb.com>
2015-06-29 18:27:52 -0400

    Move request.redirect to website
    Along with this:
    
     - restrict redirects with website.base_url if available
     - move canonicalize_base_url onto website as well
     - allow user to pass in response instance (obviates Response.redirect
       hack on Gratipay)

--- aspen/utils.py
+++ aspen/utils.py
@@ -386,47 +386,6 @@ def typecheck(*checks):
             raise TypeError(msg)
 
 
-# Base URL Canonicalization
-# =========================
-
-def _extract_scheme(request):
-    return request.headers.get('X-Forwarded-Proto', 'http')  # Heroku
-
-def _extract_host(request):
-    return request.headers['Host']  # will 400 if missing
-
-def BaseURLCanonicalizer(expected, permanent_redirect=False, extract_scheme=_extract_scheme,
-        extract_host=_extract_host):
-    """Takes a base_url such as http://localhost:8080 (no path part).
-    """
-
-    def noop(request):
-        pass
-
-    def func(request):
-        """Enforce a certain base URL.
-        """
-
-        scheme = extract_scheme(request)
-        host = extract_host(request)
-
-        actual = scheme + "://" + host
-
-        if actual != expected:
-            uri = expected
-            if request.line.method in ('GET', 'HEAD', 'OPTIONS', 'TRACE'):
-                # Redirect to a particular path for idempotent methods.
-                uri += request.line.uri.path.raw
-                if request.line.uri.querystring:
-                    uri += '?' + request.line.uri.querystring.raw
-            else:
-                # For non-idempotent methods, redirect to homepage.
-                uri += '/'
-            request.redirect(uri, permanent=permanent_redirect)
-
-    return expected and func or noop
-
-
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
