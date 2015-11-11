1afcf976147fbd2eaefd2e3d876d557da233ac61
Chad Whitacre <chad@zetaweb.com>
2015-11-11 11:21:55 -0500

    Decouple accept_header from request object

--- aspen/algorithm.py
+++ aspen/algorithm.py
@@ -105,10 +105,6 @@ def resource_available():
     pass
 
 
-def extract_accept_from_request(request):
-    return {'accept_header': request.headers.get('accept')}
-
-
 def get_response_for_resource(state, website, resource=None):
     if resource is not None:
         state.setdefault('response', Response(charset=website.charset_dynamic))
