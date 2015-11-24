9a36cc2aa9a98a5b804f19f3051449565aa0009b
Chad Whitacre <chad@zetaweb.com>
2015-11-23 21:40:23 -0500

    Prune website.redirect

--- aspen/website.py
+++ aspen/website.py
@@ -9,8 +9,6 @@ from __future__ import unicode_literals
 
 from algorithm import Algorithm
 from .configuration import Configurable
-from .http.response import Response
-from .exceptions import BadLocation
 
 
 class Website(Configurable):
@@ -39,27 +37,3 @@ class Website(Configurable):
                                  , _raise_immediately=raise_immediately
                                  , _return_after=return_after
                                   )
-
-    def redirect(self, location, code=None, permanent=False, base_url=None, response=None):
-        """Raise a redirect Response.
-
-        If code is None then it will be set to 301 (Moved Permanently) if
-        permanent is True and 302 (Found) if it is False. If url doesn't start
-        with base_url (defaulting to self.base_url), then we prefix it with
-        base_url before redirecting. This is a protection against open
-        redirects. If you wish to use a relative path or full URL as location,
-        then base_url must be the empty string; if it's not, we raise
-        BadLocation. If you provide your own response we will set .code and
-        .headers['Location'] on it.
-
-        """
-        response = response if response else Response()
-        response.code = code if code else (301 if permanent else 302)
-        base_url = base_url if base_url is not None else self.base_url
-        if not location.startswith(base_url):
-            newloc = base_url + location
-            if not location.startswith('/'):
-                raise BadLocation(newloc)
-            location = newloc
-        response.headers['Location'] = location
-        raise response
