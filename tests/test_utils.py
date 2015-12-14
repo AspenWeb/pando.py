2d169741eda27f97f1025836968ff40443918736
Chad Whitacre <chad@zetaweb.com>
2015-12-14 08:11:58 -0500

    Remove datetime helpers from utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,8 +6,7 @@ from __future__ import unicode_literals
 from pytest import raises
 
 import aspen.utils # this happens to install the 'repr' error strategy
-from aspen.utils import ascii_dammit, unicode_dammit, to_age, to_rfc822, utcnow
-from datetime import datetime, timedelta
+from aspen.utils import ascii_dammit, unicode_dammit
 
 GARBAGE = b"\xef\xf9"
 
@@ -40,25 +39,3 @@ def test_unicode_dammit_takes_encoding():
 def test_ascii_dammit_works():
     actual = ascii_dammit(b"comet: \xe2\x98\x84")
     assert actual == r"comet: \xe2\x98\x84"
-
-def test_to_age_barely_works():
-    now = utcnow()
-    actual = to_age(now, dt_now=now)
-    assert actual == "in just a moment"
-
-    wait = timedelta(seconds=0.5)
-    actual = to_age(now - wait, dt_now=now)
-    assert actual == "just a moment ago"
-
-def test_to_age_fails():
-    raises(ValueError, to_age, datetime.utcnow())
-
-def test_to_age_formatting_works():
-    now = utcnow()
-    actual = to_age(now, fmt_future="Cheese, for %(age)s!", dt_now=now)
-    assert actual == "Cheese, for just a moment!"
-
-def test_to_rfc822():
-    expected = 'Thu, 01 Jan 1970 00:00:00 GMT'
-    actual = to_rfc822(datetime(1970, 1, 1))
-    assert actual == expected
