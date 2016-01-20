ab0f6e7b2b5ec8fcd9c910e0386f5a71de03a096
Jason R. Coombs <jaraco@jaraco.com>
2016-01-20 15:33:13 -0500

    Correct Python 3 syntax errors with Python 2.6-compatible syntax.

--- aspen/auth/httpdigest.py
+++ aspen/auth/httpdigest.py
@@ -133,8 +133,8 @@ class Storage(dict):
     def __getattr__(self, key):
         try:
             return self[key]
-        except KeyError, k:
-            raise AttributeError, k
+        except KeyError as k:
+            raise AttributeError(k)
 
     def __setattr__(self, key, value):
         self[key] = value
@@ -142,8 +142,8 @@ class Storage(dict):
     def __delattr__(self, key):
         try:
             del self[key]
-        except KeyError, k:
-            raise AttributeError, k
+        except KeyError as k:
+            raise AttributeError(k)
 
     def __repr__(self):
         return '<Storage ' + dict.__repr__(self) + '>'
