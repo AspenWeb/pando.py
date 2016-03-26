be978df2cb5fb9b96d17de134d2762a84b28e954
Changaco <changaco@changaco.oy.lc>
2016-03-26 19:32:56 +0100

    import `Path` and `Querystring` from aspen

--- pando/http/request.py
+++ pando/http/request.py
@@ -36,12 +36,13 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 
-import cgi
 import re
 import sys
 import urllib
 import urlparse
 from cStringIO import StringIO
+
+from aspen.http.request import Path as _Path, Querystring as _Querystring
 
 from .. import Response
 from .baseheaders import BaseHeaders
@@ -153,16 +154,6 @@ class UnicodeWithRaw(unicode):
     def __new__(cls, raw, encoding="UTF-8"):
         obj = super(UnicodeWithRaw, cls).__new__(cls, raw.decode(encoding))
         obj.raw = raw
-        return obj
-
-class PathPart(unicode):
-    """A string with a mapping for extra data about it."""
-
-    __slots__ = ['params']
-
-    def __new__(cls, value, params):
-        obj = super(PathPart, cls).__new__(cls, value)
-        obj.params = params
         return obj
 
 
@@ -470,71 +461,17 @@ class URI(unicode):
         obj.raw = raw
         return obj
 
-def extract_rfc2396_params(path):
-    """RFC2396 section 3.3 says that path components of a URI can have
-    'a sequence of parameters, indicated by the semicolon ";" character.'
-    and that ' Within a path segment, the characters "/", ";", "=", and
-    "?" are reserved.'  This way you can do
-    /frisbee;color=red;size=small/logo;sponsor=w3c;color=black/image.jpg
-    and each path segment gets its own params.
-
-    * path should be raw so we don't split or operate on a decoded character
-    * output is decoded
-    """
-    pathsegs = path.lstrip(b'/').split(b'/')
-    def decode(input):
-        return urllib.unquote(input).decode('UTF-8')
-
-    segments_with_params = []
-    for component in pathsegs:
-        parts = component.split(b';')
-        params = Mapping()
-        segment = decode(parts[0])
-        for p in parts[1:]:
-            if '=' in p:
-                k, v = p.split(b'=', 1)
-            else:
-                k, v = p, b''
-            params.add(decode(k), decode(v))
-        segments_with_params.append(PathPart(segment, params))
-    return segments_with_params
-
 
 # Request -> Line -> URI -> Path
 
-class Path(Mapping):
-    """Represent the path of a resource.
-    """
-
-    def __init__(self, raw):
-        self.raw = raw
-        self.decoded = urllib.unquote(raw).decode('UTF-8')
-        self.parts = extract_rfc2396_params(raw)
+class Path(Mapping, _Path):
+    pass
 
 
 # Request -> Line -> URI -> Querystring
 
-class Querystring(Mapping):
-    """Represent an HTTP querystring.
-    """
-
-    def __init__(self, raw):
-        """Takes a string of type application/x-www-form-urlencoded.
-        """
-        self.decoded = urllib.unquote_plus(raw).decode('UTF-8')
-        self.raw = raw
-
-        # parse_qs does its own unquote_plus'ing ...
-        as_dict = cgi.parse_qs( raw
-                              , keep_blank_values = True
-                              , strict_parsing = False
-                               )
-
-        # ... but doesn't decode to unicode.
-        for k, vals in as_dict.items():
-            as_dict[k.decode('UTF-8')] = [v.decode('UTF-8') for v in vals]
-
-        Mapping.__init__(self, as_dict)
+class Querystring(Mapping, _Querystring):
+    pass
 
 
 # Request -> Line -> Version
