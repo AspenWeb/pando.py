94869de5bc7a6c8bfef68f94f8bc9cac6a6da6cd
Changaco <changaco@changaco.oy.lc>
2016-03-27 14:34:15 +0200

    drop tests that also exist in aspen

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,41 +5,9 @@ from __future__ import unicode_literals
 
 from pytest import raises
 
-import pando.utils # this happens to install the 'repr' error strategy
-from pando.utils import ascii_dammit, unicode_dammit, to_age, to_rfc822, utcnow
+from pando.utils import to_age, to_rfc822, utcnow
 from datetime import datetime, timedelta
 
-GARBAGE = b"\xef\xf9"
-
-
-def test_garbage_is_garbage():
-    raises(UnicodeDecodeError, lambda s: s.decode('utf8'), GARBAGE)
-
-def test_repr_error_strategy_works():
-    errors = 'repr'
-    actual = GARBAGE.decode('utf8', errors)
-    assert actual == r"\xef\xf9"
-
-def test_unicode_dammit_works():
-    actual = unicode_dammit(b"foo\xef\xfar")
-    assert actual == r"foo\xef\xfar"
-
-def test_unicode_dammit_fails():
-    raises(TypeError, unicode_dammit, 1)
-    raises(TypeError, unicode_dammit, [])
-    raises(TypeError, unicode_dammit, {})
-
-def test_unicode_dammit_decodes_utf8():
-    actual = unicode_dammit(b"comet: \xe2\x98\x84")
-    assert actual == u"comet: \u2604"
-
-def test_unicode_dammit_takes_encoding():
-    actual = unicode_dammit(b"comet: \xe2\x98\x84", encoding="ASCII")
-    assert actual == r"comet: \xe2\x98\x84"
-
-def test_ascii_dammit_works():
-    actual = ascii_dammit(b"comet: \xe2\x98\x84")
-    assert actual == r"comet: \xe2\x98\x84"
 
 def test_to_age_barely_works():
     now = utcnow()
