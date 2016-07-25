7b08464810810e75da5218644b56f341c4d23555
Changaco <changaco@changaco.oy.lc>
2016-07-25 20:20:43 +0200

    prune unused `to_age()` function, closes #494

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,28 +3,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from pytest import raises
+from pando.utils import to_rfc822
+from datetime import datetime
 
-from pando.utils import to_age, to_rfc822, utcnow
-from datetime import datetime, timedelta
-
-
-def test_to_age_barely_works():
-    now = utcnow()
-    actual = to_age(now, dt_now=now)
-    assert actual == "in just a moment"
-
-    wait = timedelta(seconds=0.5)
-    actual = to_age(now - wait, dt_now=now)
-    assert actual == "just a moment ago"
-
-def test_to_age_fails():
-    raises(ValueError, to_age, datetime.utcnow())
-
-def test_to_age_formatting_works():
-    now = utcnow()
-    actual = to_age(now, fmt_future="Cheese, for %(age)s!", dt_now=now)
-    assert actual == "Cheese, for just a moment!"
 
 def test_to_rfc822():
     expected = 'Thu, 01 Jan 1970 00:00:00 GMT'
