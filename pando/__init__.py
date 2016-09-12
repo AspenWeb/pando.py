1f01403ba4b0072f0b14b4c810758f55a514fee8
Changaco <changaco@changaco.oy.lc>
2016-09-12 17:38:48 +0200

    allow running pando without installing it

--- pando/__init__.py
+++ pando/__init__.py
@@ -56,6 +56,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from os.path import dirname, join
 import sys
 import pkg_resources
 
@@ -71,8 +72,13 @@ from .logging import log, log_dammit
 # Shut up, PyFlakes. I know I'm addicted to you.
 Response, json, log, log_dammit, BUILTIN_RENDERERS, RENDERERS
 
-dist = pkg_resources.get_distribution('pando')
-__version__ = dist.version
+try:
+    dist = pkg_resources.get_distribution('pando')
+    __version__ = dist.version
+except pkg_resources.DistributionNotFound:
+    with open(join(dirname(dirname(__file__)), 'version.txt')) as f:
+        __version__ = f.read()
+
 WINDOWS = sys.platform[:3] == 'win'
 
 is_callable = callable
