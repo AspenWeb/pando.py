810559f011609112f2e781fccc2dc307144d9463
Changaco <changaco@changaco.oy.lc>
2019-05-25 17:56:14 +0200

    adapt the tests to Aspen's new dispatcher

--- tests/test_request_body.py
+++ tests/test_request_body.py
@@ -10,13 +10,12 @@ from pytest import raises
 from pando.exceptions import MalformedBody, UnknownBodyType
 from pando.http.request import Request
 from pando.http.response import Response
-from pando.website import Website
 
 
 FORMDATA = object()
 WWWFORM = object()
 
-def make_body(raw, headers=None, content_type=WWWFORM):
+def make_body(harness, raw, headers=None, content_type=WWWFORM):
     if not isinstance(raw, bytes):
         raw = raw.encode('ascii')
     if headers is None:
@@ -26,17 +25,18 @@ def make_body(raw, headers=None, content_type=WWWFORM):
     if not b'Content-Length' in headers:
         headers[b'Content-Length'] = str(len(raw)).encode('ascii')
     headers[b'Host'] = b'Blah'
-    website = Website()
+    website = harness.client.website
     request = Request(website, body=BytesIO(raw), headers=headers)
     return request.body
 
 
-def test_body_is_unparsed_for_empty_content_type():
+def test_body_is_unparsed_for_empty_content_type(harness):
     raw = "cheese=yes"
-    raises(UnknownBodyType, make_body, raw, headers={})
+    with raises(UnknownBodyType):
+        make_body(harness, raw, headers={})
 
-def test_body_barely_works():
-    body = make_body("cheese=yes")
+def test_body_barely_works(harness):
+    body = make_body(harness, "cheese=yes")
     actual = body['cheese']
     assert actual == "yes"
 
@@ -54,38 +54,39 @@ Content-Type: text/plain
 --AaB03x--
 """.splitlines())
 
-def test_body_barely_works_for_form_data():
-    body = make_body(UPLOAD, content_type=FORMDATA)
+def test_body_barely_works_for_form_data(harness):
+    body = make_body(harness, UPLOAD, content_type=FORMDATA)
     actual = body['files'].filename
     assert actual == "file1.txt"
 
-def test_simple_values_are_simple():
-    body = make_body(UPLOAD, content_type=FORMDATA)
+def test_simple_values_are_simple(harness):
+    body = make_body(harness, UPLOAD, content_type=FORMDATA)
     actual = body['submit-name']
     assert actual == "Larry"
 
-def test_multiple_values_are_multiple():
-    body = make_body("cheese=yes&cheese=burger")
+def test_multiple_values_are_multiple(harness):
+    body = make_body(harness, "cheese=yes&cheese=burger")
     assert body.all('cheese') == ["yes", "burger"]
 
-def test_params_doesnt_break_www_form():
-    body = make_body("statement=foo"
-                    , content_type=b"application/x-www-form-urlencoded; charset=UTF-8; cheese=yummy"
-                     )
+def test_params_doesnt_break_www_form(harness):
+    body = make_body(
+        harness, "statement=foo",
+        content_type=b"application/x-www-form-urlencoded; charset=UTF-8; cheese=yummy"
+    )
     actual = body['statement']
     assert actual == "foo"
 
-def test_malformed_body_jsondata():
+def test_malformed_body_jsondata(harness):
     with raises(MalformedBody):
-        make_body("foo", content_type=b"application/json")
+        make_body(harness, "foo", content_type=b"application/json")
 
-def test_malformed_body_formdata():
+def test_malformed_body_formdata(harness):
     with raises(MalformedBody):
-        make_body("", content_type=b"multipart/form-data; boundary=\0")
+        make_body(harness, "", content_type=b"multipart/form-data; boundary=\0")
 
-def test_bad_content_length():
+def test_bad_content_length(harness):
     with raises(Response) as x:
-        make_body("{}", headers={
+        make_body(harness, "{}", headers={
             b'Content-Length': b'NaN',
             b'Content-Type': b'application/json',
         })
